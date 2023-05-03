5db96c811c39a91e1aca2c5b1f48a978d7593d16
aokmen <abdullah.okmen.11@gmail.com>
2023-05-03 10:42:40 +0200

    app_js_added

---
+++ JS-01/app.js
@@ -0,0 +1,10 @@
+//? dfg
+// todo 
+//! importing 
+console.log("This is a console line")
+console.error("This is error")
+console.warn("This a warn")
+console.clear();
+console.table([{name:"Tom",age:30},{name:"Bill",age:38}])
+alert("This a alert")
+document.write("Show in browser")
