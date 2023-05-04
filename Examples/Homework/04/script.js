8b4132ce8fdf762a68f18243b2c9b9ad0a12660a
aokmen <abdullah.okmen.11@gmail.com>
2023-05-04 19:33:59 +0200

    javascript.js_added

---
+++ Examples/Homework/04/script.js
@@ -0,0 +1,21 @@
+function chang(){
+    // document.getElementById("title1").innerHTML="you may proceed";
+            // document.getElementById("title1").style.visibility="visible"; 
+            document.getElementById("title1").style.color="darkgreen";
+            document.getElementById("title2").style.color="darkcyan"; 
+            document.body.style.backgroundImage="url('./img/camping.jpg')"
+}
+function chang2(){
+    // document.getElementById("title1").innerHTML="you may proceed";
+            // document.getElementById("title1").style.visibility="visible";  
+            document.getElementById("title1").style.color="darkcyan";
+            document.getElementById("title2").style.color="darkgreen"; 
+            document.body.style.backgroundImage="url('./img/stone.jpg')"
+}
+function chang3(){
+    // document.getElementById("title1").innerHTML="you may proceed";
+            // document.getElementById("title1").style.visibility="visible";  
+            document.getElementById("title1").style.color="darkred";
+            document.getElementById("title2").style.color="coral"; 
+            document.body.style.backgroundImage="url('./img/tree.jpg')"
+}
