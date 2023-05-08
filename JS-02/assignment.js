8ce5081c682ab6cbeae1a9b15ac122a595479713
aokmen <abdullah.okmen.11@gmail.com>
2023-05-08 15:01:52 +0200

    updated

--- JS-02/assignment.js
+++ JS-02/assignment.js
@@ -37,3 +37,54 @@ console.log(text6);
 console.log( typeof text6);
 console.log(parseFloat(text6));
 console.log( typeof parseFloat(text6));
+
+//7
+let text7 = "58694.256";
+console.log("7----------");
+console.log(text7);
+console.log( typeof text7);
+console.log(Math.round(text7));
+
+//8
+let text8 = "58694.256";
+console.log("8----------");
+console.log(text8);
+console.log(typeof text8);
+console.log(parseInt(text8));
+console.log(typeof parseInt(text8));
+
+//9
+let text9 = "58694.256";
+console.log("9----------");
+console.log(text9);
+console.log(60 + parseFloat(text9));
+
+//10
+let text10 = Math.round(Math.random()*20);
+console.log("10----------");
+console.log(text10);
+
+//11
+let text11 = Math.round(Math.random()*10);
+console.log("11----------");
+console.log(text11);
+
+//12
+let text12 = Math.round(Math.random()*100);
+console.log("12----------");
+console.log(text12);
+
+//13
+let text13 = Math.round(Math.random()*10);
+console.log("13----------");
+console.log(text13);
+
+//14
+let text14 = Math.max(10,11,12,13);
+console.log("14----------");
+console.log(text14);
+
+//15
+let text15 = Math.min(10,11,12,13);
+console.log("15----------");
+console.log(text15);
