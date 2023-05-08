51db1793c4b008853122f96e71666aaa4b998a96
aokmen <abdullah.okmen.11@gmail.com>
2023-05-08 14:07:50 +0200

    new

---
+++ JS-02/assignment.js
@@ -0,0 +1,39 @@
+console.clear();
+
+//1
+let text1 = 345;
+console.log("1----------");
+console.log(text1);
+console.log( typeof text1);
+
+//2
+let text2 = "Hello Clarusway DE-06"
+console.log("2----------");
+console.log(text2);
+console.log( typeof text2);
+
+//3
+let text3 = true;
+console.log("3----------");
+console.log(text3);
+console.log( typeof text3);
+
+//4
+let text4;
+console.log("4----------");
+console.log(text4);
+console.log( typeof text4);
+
+//5
+let text5 = null;
+console.log("5----------");
+console.log(text5);
+console.log( typeof text5);
+
+//6
+let text6 = "58694.256";
+console.log("6----------");
+console.log(text6);
+console.log( typeof text6);
+console.log(parseFloat(text6));
+console.log( typeof parseFloat(text6));
