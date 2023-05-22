ec383e7a183dddd015e9c49ddcfb9b639f26c16d
aokmen <abdullah.okmen.11@gmail.com>
2023-05-22 11:34:55 +0200

    new_functions_coding_added

--- JS-07/function.js
+++ JS-07/function.js
@@ -92,4 +92,90 @@ function tamadi (adi,soyadi=""){  //! Varsayilan bir deger atanabilir...
 console.log(tamadi("ali"));
 console.log(tamadi("ali","veli"));
 
+//***************************** */
 
+console.clear();
+
+function result1(num1,num2){
+    return num1**num2;
+}
+
+console.log(result1(5,6));
+console.clear();
+
+function result1(num1,num2){
+    return num1**num2;
+}
+
+console.log(result1(5,6));
+console.clear();
+
+//---------------
+let tarih = new Date();
+let yil = tarih.getFullYear();
+
+function yas(yas){
+    return yil-yas;
+}
+
+console.log(yas(2010));
+console.clear();
+// const date = new Date();
+// const year = date.getFullYear();
+
+// function yas(birthYear){
+//     return year - birthYear;
+// }
+
+// console.log(age(1980));
+//dik(4,3);
+// ****
+// ****
+// ****
+
+let ilk =""
+function dik(n1,n2){
+    for(i=0 ; i<n2; i++){
+        for(j=0 ; j<n1; j++){
+            ilk +="*";
+        } 
+        ilk += "\n";
+       
+        
+    } console.log(ilk);
+}
+dik(4,3);
+
+// for (let i = 0; i < 6; i++) {
+//     for (let j = 0; j < 6; j++) {
+//       star +="* ";
+//     }
+//     star += "\n";
+// }
+// console.log(star);
+
+let gun = new Date();
+console.log(gun);
+let ay = gun.getFullYear();
+
+console.clear();
+//---------------------------------------
+
+function oyun(ad,seviye,puan){
+
+    if (seviye=="VE"){
+       puan +=puan*0.1;
+    }
+    else if (seviye=="ME"){
+        puan +=puan*0.2;
+     }
+     else if (seviye=="HR"){
+        puan +=puan*0.3;
+     }
+     else if (seviye=="EX"){
+        puan +=puan*0.5;
+     }
+        return `Adi: ${ad}, Seviye: ${seviye}, Puani: ${puan}`
+}
+
+console.log(oyun("Ali","ME",100));
