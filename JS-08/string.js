fe99a3d1f0b1153514cdac6ef1aa3f7ed1202dd8
aokmen <abdullah.okmen.11@gmail.com>
2023-05-31 09:16:44 +0200

    string_js_updated

--- JS-08/string.js
+++ JS-08/string.js
@@ -125,6 +125,9 @@ let yenisoz= atasoz.split(" ");
      : null;
  }
 }
+let ff = "sen bir gun ama geleceksin";
+let dd = ff.split(" ");
+for (let i = 0; i < dd.length; i++)  {(dd[i]==="ama1") ? console.log("evet") : null;}
 
 console.log(ayir("ama"));
 
@@ -159,7 +162,7 @@ const text7 = (yazi) => console.log(yazi.split("").reverse().join(""));
 text7("Javascript");
 
 /* -------------------------------------------------------------------------- */
-/*5-                                 Slice()                                  */
+/*                                   Slice()                                  */
 /* -------------------------------------------------------------------------- */
 
 // slice(başlangıç index numarası , bitiş index numarası ) başlangıç ve bitiş arasındaki karakterleri alır
@@ -173,3 +176,186 @@ console.log(message.slice(3,5));  //ca
 console.log(message.slice(3));     //calama
 
 console.log(message.slice(-5,-3)); //al
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                 substring()                                */
+/* -------------------------------------------------------------------------- */
+// substring(başlangıç index numarası , bitiş index numarası ) başlangıç ve bitiş arasındaki karakterleri alır
+// slice metodu ile tek fark - negatif sayi degeri kullanilmaz yani sondan baslayamaz
+
+console.log(message.substring(6,8));
+
+/* -------------------------------------------------------------------------- */
+/*                                 substr()   - Deprecated                             */
+/* -------------------------------------------------------------------------- */
+// substr(başlangıç index numarası ,kac adet karakter alacak)
+
+console.log(message.substr(3,5));  // 5 kac adet karakter sayisi
+
+/* -------------------------------------------------------------------------- */
+/*                                 replace()                             */
+/* -------------------------------------------------------------------------- */
+// String degerinde degisiklik yapma yerine yazma
+
+ message="Tecrübe tarak gibidir; ama hayat insana kel olduğu zaman verir.";
+ console.log(message.replace("kel","ihtiyaci"));  // tek bir bosluk degisir
+
+ console.log(message.replaceAll(" ","-")); // tüm bolsluklar degisir
+
+ let variable = "kullanıcı adı";
+
+ console.log(variable.replaceAll("ı","i").replace(" ", "_").toUpperCase());
+
+
+/* -------------------------------------------------------------------------- */
+/*                                 search()                                   */
+/* -------------------------------------------------------------------------- */
+// includes, indexOf, search, match gibi benzer metotlarla cumle veya kelime icinde karakterleri bulma
+
+//? includes() metodu "..." degerini iceriyor mu? => true ya da false döndürür(casesensitive)
+
+console.log(message.includes("kel"));
+
+message.includes("zaman") ? console.log("bu cümle icinde zaman kelimesi gecer") : console.log("bu cümle icinde zaman kelimesi icermez");
+
+let url = "https://www.clarusway.com";
+url.includes("https") ? console.log("güvenli bir site") : console.log("guvenli degil");
+
+let mail = "clarusway@web.com";
+mail.includes("@") ? console.log("bu bir mail adresidir") : console.log("bu bir mail adresidi degil");
+
+const adress = (mail) => console.log(mail.includes("@"));
+
+adress("clarusway@web.com");
+
+
+//--------------------------------------indexOf--------------------------
+
+//? indexOf(aranacak metin, konum) Bir karakter yada karakter grubumun kaçıncı sırada olduğunu yani index numarasını verir. Eğer o karakter yoksa olmadığını -1 değeri vererek gösterir. Büyük küçük harfe duyarlıdır. Sadece ilk gördüğünün index numarasını verir
+
+console.log(message.indexOf("kel"));  //40
+console.log(message.indexOf("keli"));  //-1 (ifade olmadigi icin)
+word="yenigün";
+console.log(word.indexOf("i",4)); //-1 (ifade olmadigi icin)
+
+//? search metodu > metin icinde baska bir metni arar. Bulmazsa -1 döner.
+console.log(message.search("hayat"));
+//regex > regular expressions : search metodunda regex kullanilabilir
+
+word="yenigün";
+// /i : büyük kücük harf farketmez
+// /g :global olarak arama yapar
+console.clear();
+console.log(word.search(/Ü/i));
+
+let kelimeKontrol = "Bu cümlede türkce karakterler vardir"
+
+console.log(kelimeKontrol.search(/[aeioöü]/i));
+
+console.log(kelimeKontrol.replaceAll(/[üc]/gi,"")); 
+
+/* -------------------------------------------------------------------------- */
+/*                                 trim()                                     */
+/* -------------------------------------------------------------------------- */
+
+// bastan ve sondan bosluklari siler
+
+let cumle = "   Clarus Full Stack    ";
+console.log(cumle.trim());
+console.log(cumle.trimStart());
+console.log(cumle.trimEnd());
+
+/* -------------------------------------------------------------------------- */
+/*                                 startsWith()                               */
+/* -------------------------------------------------------------------------- */
+
+// startsWith() ve endsWith() bastan veya sondan boyle bir karakter var mi? true ve false döndürür
+metin = "Clarusway";
+
+ console.log(metin.startsWith("C"));  //true
+ console.log(metin.startsWith("Cl"));  //true
+ console.log(metin.endsWith("ayi"));  //false
+
+console.clear();
+console.log(word.search(/Ü/i));
+
+
+
+kelimeKontrol = "Bu cumlede türkce karakterler vardir"
+
+console.log(kelimeKontrol.search(/[aueioöü]/i));
+
+console.log(kelimeKontrol.replaceAll(/[üc]/gi,"")); 
+
+console.clear();
+
+// -----------------------------------1- Hosgeldin Kullanici Adi--------------------------
+
+
+const user = () => {
+    let ad = prompt("Kullanici adini giriniz:")
+    return `${ad.toUpperCase()} Sitemize Hoşgeldiniz`
+}
+ console.log(user());
+
+// -----------------------------------2- Romen Rakami-------------------------------------
+const cevir = (romen) =>{
+    romen = romen.split("");
+    let sum = 0;
+    for (let i = 0; i < romen.length; i++) {
+    
+    
+        switch (romen[i]) {
+            case "X":
+             sum +=10;   
+                break;
+            case "V":
+             sum +=5;   
+                break;
+            case "I":
+             sum +=1;   
+                break;
+    
+            default:
+                break;
+        }
+        
+    }
+    return sum;
+}
+
+console.log(cevir("XXV"));
+// -----------------------------------3- Email Kullanici Adi------------------------------
+
+// let email = "martin@clarusway.com";
+// console.log((email.split("@"))[0]);
+
+// -----------------------------------4- Kelimeyi substring ayirma------------------------
+
+// function Substrings(word) {
+//     let word2="";
+//     for (var i = 0; i < word.length; i++) {
+//       for (var j = i + 1; j <= word.length; j++) {
+//        word2 += (word.slice(i, j)).concat(" ");
+//       } 
+//     }
+//     return word2;
+//   }
+
+//   console.log(Substrings("abc"));
+  
+//   console.clear();
+
+// -----------------------------------5- Kelimeyi camelcase yapma------------------------
+
+// let cumlem = "Bugün günesli bir gün";
+//  words = cumlem.toLowerCase().split(" ");
+// for (let i = 1; i < words.length; i++) {
+//     words[i] = words[i][0].toUpperCase() + words[i].slice(1);  
+// }
+
+// console.log(words.join(""));
+
+
