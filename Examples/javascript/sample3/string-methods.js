5a81eacf37f8b5f2bb3ff663cf6fbe495b033d7c
aokmen <abdullah.okmen.11@gmail.com>
2023-06-10 09:10:21 +0200

    map_filter_js

--- Examples/javascript/sample3/string-methods.js
+++ Examples/javascript/sample3/string-methods.js
@@ -5,10 +5,8 @@
 
 
 
-// Bir dizi oluşturun ve dizinin elemanlarını alfabetik olarak sıralayın.
-// Bir dizi oluşturun ve dizinin elemanlarını harf sayısına göre sıralayın.
-// Bir dizi oluşturun ve dizinin elemanlarını ters sıralayın.
-// Bir dizi oluşturun ve dizinin elemanlarının toplamını hesaplayın, ancak 3'ün katları olan elemanları hesaplamaya dahil etmeyin.
+
+
 
 // Bir dizi oluşturun ve dizi içindeki tüm sayıları toplayın.
 let x = "Ali";
@@ -19,26 +17,26 @@ console.log(arraySum);
 
 // Bir dizi oluşturun ve dizinin elemanlarını tersine çevirin.
 
-array2 = ["Animal","Cat","Dog","Horse","Leon"];
+let array2 = ["Animal","Cat","Dog","Horse","Leon"];
 
 const arrayReverse = array2.map((item) => item.split("").reverse().join(""));
 console.log(arrayReverse);
 
 // Bir dizi oluşturun ve dizideki çift sayıları filtreleyin.
 
-array3 = [2,6,8,4,9,10,24];
+let array3 = [2,6,8,4,9,10,24];
 const arrayEven = array3.filter((item) => item % 2===0)
 console.log(arrayEven);
 
 // Bir dizi oluşturun ve dizideki tüm string değerleri büyük harflere dönüştürün.
 
-array4 = ["Animal","Cat","Dog","Horse","Leon"];
+let array4 = ["Animal","Cat","Dog","Horse","Leon"];
 const arrayUpper = array4.map((arg) => arg.toUpperCase())
 console.log(arrayUpper);
 
 // Bir dizi oluşturun ve dizideki elemanların toplamını hesaplayın, ancak 5'ten büyük olan elemanları hesaplamaya dahil etmeyin.
 
-array5 = [2,6,8,4,9,10,3,24];
+let array5 = [2,6,8,4,9,10,3,24];
 
 const fiveFilter = array5.filter((arg) => arg < 5).reduce((sum,item) => sum += item);
 
@@ -46,15 +44,15 @@ console.log(fiveFilter);
 
 // Bir dizi oluşturun ve dizideki tek sayıları filtreleyin, ardından her bir elemanın karesini alın.
 
-array6 = [2,6,8,4,9,10,3,24,11,15];
+let array6 = [2,6,8,4,9,10,3,24,11,15];
 
 const arrayOdd = array6.filter((arg) => arg%2===1).map((arg) => arg**2)
 console.log(arrayOdd);
 
 //for in Method
 
-array8=[];
-array7 = [2,6,8,4,9,10,3,24,11,15];
+let array8=[];
+let array7 = [2,6,8,4,9,10,3,24,11,15];
 for ( i in array7) {
     if (array7[i]%2===1) {
         array8.push(array7[i]**2);
@@ -65,17 +63,43 @@ console.log("for in:",array8);
 
 //for of Method
 
-array10=[];
-array9 = [2,6,8,4,9,10,3,24,11,15];
+let array10=[];
+let array9 = [2,6,8,4,9,10,3,24,11,15];
 
 for ( item of array9) item % 2 === 1 ? array10.push(item**2) : null;
 console.log("for of:",array10);
 
 //for Each Method
 
-array12=[];
-array11 = [2,6,8,4,9,10,3,24,11,15];
+let array12=[];
+let array11 = [2,6,8,4,9,10,3,24,11,15];
 
-array11.forEach(element,index => { 
-    console.log(element,index);
+array11.forEach((element,index) => { 
+    element%2===1 ? array12.push(element**2) : null;
 });
+console.log("forEach",array12);
+
+// Bir dizi oluşturun ve dizinin elemanlarını alfabetik olarak sıralayın.
+
+let array13 = [2,6,8,4,9,10,3,24,11,15];
+let array14 = ["Animal","Cat","Dog","Horse","Leon"];
+
+const arrayOrder = array13.sort((a,b)=>a-b);
+console.log(arrayOrder);
+
+const textOrder = array14.sort();
+console.log(textOrder);
+// Bir dizi oluşturun ve dizinin elemanlarını harf sayısına göre sıralayın.
+let array15 = ["Animal","Xat","Dog","Horse","Leon"];
+
+let letterOrder = array15.sort((a,b)=>b.length-a.length);
+console.log(letterOrder);
+
+// Bir dizi oluşturun ve dizinin elemanlarını ters sıralayın.
+
+let array16 = [2,6,8,4,9,10,3,24,11,15];
+
+const numOrder = array16.sort((a,b)=>b-a);
+console.log(numOrder);
+
+// Bir dizi oluşturun ve dizinin elemanlarının toplamını hesaplayın, ancak 3'ün katları olan elemanları hesaplamaya dahil etmeyin.
