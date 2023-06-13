257e905453226c4d3e5f84c15f9d244eef1bf027
aokmen <abdullah.okmen.11@gmail.com>
2023-06-13 17:08:15 +0200

    new_feat_3_added

--- JS-12/New_Generation.js
+++ JS-12/New_Generation.js
@@ -169,3 +169,136 @@ workersList.map((p)=> checkDriving(p))
 const nameList =["Ayse","Elif"]
 const[p1,p2]=nameList
 console.log(p1);
+
+/* -------------------------------------------------------------------------- */
+/*                                    REST (...)                              */
+/* -------------------------------------------------------------------------- */
+
+// fonksiyonlarda islemistik
+// function decleration cok fazla gönderilen parametreleri => arguments
+function argCheck() {
+  console.log(arguments);
+}
+argCheck(45,7,44,9,4,"Merhaba")
+
+/* ------------------------------------ - ----------------------------------- */
+
+// arrow funtionlarda ...(rest) gelen parametreleri almak icin kullanilir.
+// fonksiyona gelen primitive degerleri de bir diziye dönüstürmüs olu.
+const arrowCheck=(a,b,...deger)=>{
+  console.log(a);
+  console.log(b);
+  console.log(deger);
+}
+arrowCheck(45,7,44,9,4,"Merhaba")
+
+/* -------------------------------------------------------------------------- */
+//Rest parameter must be last formal parameter
+// rest operatoru mutlaka sonda olmalı
+// const arrowCheck=(...deger,a,b)=>{
+//   console.log(a);
+//   console.log(b);
+//   console.log(deger);
+//   }
+  
+//   arrowCheck(45,56,7,8,34,5,6,7,8,"Merhaba")
+
+//? Rest Operatörünün Arraylerde kullanimi
+
+const car=["BMW","Mercedes","audi","renault","ferrari","opel","Mazda","togg"];
+
+const [a,b,...restCars] = car;
+
+console.log(a);
+console.log(b);
+console.log(restCars);
+
+//? Rest Operatörünün Objectlerde kullanimi
+
+personel = 
+    {
+      pName: "David",
+      lastname: "Strevy",
+      DateOfBirth: 2000,
+      job: "developer",
+      salary: 8000,
+      drivingLicense: true,
+    }
+
+const {pName,lastname,...info} = personel;
+console.log(`personelimizin adi ${pName}`);
+console.log(`personelimizin soyadi ${lastname}`);
+
+console.log(info);  // Console da her zaman icerinin ne tür oldugunu bilmek lazim >> Bu bir Object
+
+console.log(info.DateOfBirth);
+console.log(info.drivingLicense);
+
+const {...hepsi}=personel; 
+console.log(hepsi.pName);
+console.log(hepsi.job);
+console.log(hepsi["pName"]);
+console.log(hepsi["job"]);
+
+/* ----------------------------------- --- ---------------------------------- */
+
+const total =(...deger)=>{
+  return deger.reduce((sum,i) => sum+i)
+}
+console.log(total(42,12,55,3,8,2));
+
+/* -------------------------------------------------------------------------- */
+/*                             SPREAD ISLEMI (...)                            */
+/* -------------------------------------------------------------------------- */
+
+// Array concatanation
+
+const flyingVehicles = ["aircraft", "helicopter", "drone"];
+const automobile = ["truck", "suv", "Car"];
+
+const allVehicles = [flyingVehicles,automobile]; // [Array(3), Array(3)]
+console.log(allVehicles);
+
+const allVehicles2 = [...flyingVehicles,...automobile] // ['aircraft', 'helicopter', 'drone', 'truck', 'suv', 'Car']
+console.log(allVehicles2);
+
+const flyingVehicles2 = ["aircraft", "helicopter", "drone"];
+const automobile2 = ["truck", "suv", "Car"];
+
+// Araya bu sekilde eklemeler de yapilir
+const allVehicles3 = [...flyingVehicles,"bicyle",...automobile]
+console.log(allVehicles3);
+
+//stringi harf harf parcalamak
+let statement = "Havalar cok sicak";
+const charSt = [...statement];
+console.log(charSt);
+
+const str = [..."Piknik"];
+console.log(str);
+
+/* -------------------------------------------------------------------------- */
+console.log( Math.max(3,5,7,9,12,3));
+
+const numberList = [3,5,7,9,12,3];
+console.log(Math.max(...numberList));
+
+
+ /* ------------------------- Object'leri birlestirme ------------------------ */
+const myObj={ a:4,b:6}
+const yourObj={c:8, d:9}
+console.log(myObj);
+// Object kopyalama icin kullanilabilir
+
+const copiedObj = {...myObj};
+console.log(copiedObj);
+
+copiedObj.b=34;
+
+console.log(myObj);
+console.log(copiedObj);
+
+ /* ------------------------- Object'leri birlestirme ------------------------ */
+
+const combinedObj ={...myObj,...yourObj};
+console.log(combinedObj);
