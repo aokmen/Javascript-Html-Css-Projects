04caf2c1d39653615b18a4185a355be550fe34ba
aokmen <abdullah.okmen.11@gmail.com>
2023-06-20 09:01:02 +0200

    getElementById_added

--- Examples/javascript/sample6/tas-makas.js
+++ Examples/javascript/sample6/tas-makas.js
@@ -1,5 +1,7 @@
 const comH1 = document.querySelector("#comH1");
 const userH1 = document.querySelector("#userH1");
+const puanC = document.querySelector("#puanC");
+const puanU = document.querySelector("#puanU");
 const tas = "Tas";
 const makas = "Makas";
 const kagit = "Kagit";
@@ -42,8 +44,17 @@ let ber = "Berabere Kaldiniz :|"
 const results = () => {
 let u = userH1.innerHTML;
 let c = comH1.innerHTML;
-if(u===tas && c===tas || u===makas && c===makas || u===kagit && c===kagit) win.innerHTML=ber;
-if(u===tas && c===kagit || u===makas && c===tas || u===kagit && c===makas) win.innerHTML=kay;
-if(u===kagit && c===tas || u===tas && c===makas || u===makas && c===kagit) win.innerHTML=kaz;
+if(u===tas && c===tas || u===makas && c===makas || u===kagit && c===kagit) 
+if(u===tas && c===kagit || u===makas && c===tas || u===kagit && c===makas) {
+    puanU.innerHTML--; 
+    puanC.innerHTML++;  }  
+
+if(u===kagit && c===tas || u===tas && c===makas || u===makas && c===kagit)
+{puanU.innerHTML++; 
+puanC.innerHTML--; }
+
+if(puanU.innerHTML==0) {win.innerHTML=kay;}
+if(puanU.innerHTML==10) {win.innerHTML=kaz;}
+
 }
 
